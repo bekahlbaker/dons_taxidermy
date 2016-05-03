f52ee1458e0093b13860703b18a8221650146259
Bekah Baker <bekahlbaker@gmail.com>
2016-05-03 08:54:01 -0500

    install

---
+++ wp-content/themes/dons_taxidermy_theme/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="IE=11,IE=10,IE=9">
+<title>
+  <?php
+    if( ! is_home() ):
+      wp_title( '|', true, 'right' );
+    endif;
+    bloginfo( 'name' );
+  ?>
+</title>
+<?php wp_head(); ?>
+</head>
+
+<!--[if IE 9]>
+ <body class="ie9">
+<![endif]-->
+<!--[if !IE]>-->
+<body <?php body_class(); ?>>
+<!--<![endif]-->
