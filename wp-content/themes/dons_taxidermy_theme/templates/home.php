c49e4a6516e4f97080ff494e37e3718750a99cef
Bekah Baker <bekahlbaker@gmail.com>
2016-05-24 12:00:55 -0500

    slider stuff

--- wp-content/themes/dons_taxidermy_theme/templates/home.php
+++ wp-content/themes/dons_taxidermy_theme/templates/home.php
@@ -6,7 +6,7 @@ Template Name: Home Template
 <?php get_header(); ?>
 
 <div id="home">
-	<div class="container">
+	<div class="container home">
 		<div class="welcome">
 			<h2 class="header"><?php the_field('welcome') ?></h2>
 			<div class="message">
@@ -16,8 +16,11 @@ Template Name: Home Template
 		<hr>
 		<div class="about">
 			<h2 class="header"><?php the_field('about_click') ?></h2>
-			<div class="message">
-				<?php the_field('about_short_message') ?>
+			
+				<img src="<?php bloginfo('template_directory'); ?>/assets/img/hunter.png" alt="" style="width: 200px;">
+				<div class="message">
+					<?php the_field('about_short_message') ?>
+			
 			</div>
 			<div class="button">
 				<a href="<?php bloginfo('url'); ?>/about"><button>Read More</button></a>
@@ -26,6 +29,21 @@ Template Name: Home Template
 		<hr>
 		<div class="my-work">
 			<h2 class="header"><?php the_field('my_work_click') ?></h2>
+			<div class="my-slider">
+
+					<div id="slider">
+					  <a href="javascript://" class="control_next">>></a>
+					  <a href="javascript://" class="control_prev"><</a>
+					  <ul>
+					    <li style="background-image: url(<?php bloginfo('template_directory'); ?>/assets/img/forest2.jpg); background-size: cover; background-position: center;"></li>
+					    <li style="background-image: url(<?php bloginfo('template_directory'); ?>/assets/img/forest.jpg); background-size: cover; background-position: center;"></li>
+					    <li style="background-image: url(<?php bloginfo('template_directory'); ?>/assets/img/forest3.jpg); background-size: cover; background-position: center;"></li>
+					    <li style="background-image: url(<?php bloginfo('template_directory'); ?>/assets/img/301H.jpg); background-size: cover; background-position: center;"></li>
+					  </ul>  
+					</div>
+
+				
+			</div>
 			<div class="message">
 				<?php the_field('my_work_short_message') ?>
 			</div>
